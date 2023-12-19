950d934826370a5a8611a0d21d4470ec5e1d67dd
NikolasFMS <109710285+NikolasFMS@users.noreply.github.com>
2023-12-19 18:34:24 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,52 @@
+
+// Функция броска костей
 function rollDice() {
     var dice1 = document.querySelector('.dice1');
     var dice2 = document.querySelector('.dice2');
 
-    // Set a random duration between 2 and 4 seconds
+    // Задаем случайную длительность между 2 и 4 секундами
     var duration = Math.floor(Math.random() * 2000) + 2000;
 
-    // Roll the dice for the random duration
+    // Бросаем кости на случайную длительность
     rollSingleDice(dice1, duration);
     rollSingleDice(dice2, duration);
 }
 
+// Функция броска одной кости
 function rollSingleDice(dice, duration) {
     var startTime = Date.now();
-    var interval = 100; // Update every 100 milliseconds
+    var interval = 100; // Обновление каждые 100 миллисекунд
     playSound();
 
-    // Update the dice image at intervals
+    // Обновляем изображение кости через интервалы времени
     var updateInterval = setInterval(function () {
         var currentTime = Date.now();
         var elapsedTime = currentTime - startTime;
 
-        // Generate a random number between 1 and 6
+        // Генерируем случайное число от 1 до 6
         var randomNum = Math.floor(Math.random() * 6) + 1;
 
-        // Update the content of the dice element with the corresponding image
+        // Обновляем содержимое элемента кости соответствующим изображением
         dice.innerHTML = `<img src="pictures/${randomNum}.svg" alt="${randomNum}">`;
 
-        // Check if the duration has passed
+        // Проверяем, прошла ли заданная длительность
         if (elapsedTime >= duration) {
-            clearInterval(updateInterval); // Stop updating the dice image
-            playEndSound(); // Play the end sound
+            clearInterval(updateInterval); // Прекращаем обновление изображения кости
+            playEndSound(); // Проигрываем звук окончания
         }
     }, interval);
 }
-        // Function to play the end sound
-        function playEndSound() {
-            var audio = document.getElementById("clickSoundEnd");
-            if (audio.readyState !== 4) {
-                audio.load();
-            }
-            audio.play();
-        }
 
-// Function to play the click sound
+// Функция для проигрывания звука окончания
+function playEndSound() {
+    var audio = document.getElementById("clickSoundEnd");
+    if (audio.readyState !== 4) {
+        audio.load();
+    }
+    audio.play();
+}
+
+// Функция для проигрывания звука начала
 function playSound() {
     var audio = document.getElementById("clickSoundStart");
     if (audio.readyState !== 4) {
@@ -50,4 +54,3 @@ function playSound() {
     }
     audio.play();
 }
-
